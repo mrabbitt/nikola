320f684c769ee4c701b1574379787cede9d91b23
Michael Rabbitt <rabbim@alum.rpi.edu>
2013-01-22 00:56:53 -0500

    Refactored Nikola-specific Markdown customization into "nikola_md" Markdown extension.

--- nikola/plugins/compile_markdown/__init__.py
+++ nikola/plugins/compile_markdown/__init__.py
@@ -8,11 +8,11 @@
 # distribute, sublicense, and/or sell copies of the
 # Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice
 # shall be included in all copies or substantial portions of
 # the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
 # KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
 # WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
@@ -26,10 +26,10 @@
 
 import codecs
 import os
-import re
 
 try:
     from markdown import markdown
+    # from nikola.plugins.compile_markdown import nikola
 except ImportError:
     markdown = None
 
@@ -41,6 +41,8 @@ class CompileMarkdown(PageCompiler):
 
     name = "markdown"
 
+    extensions = ['fenced_code', 'codehilite', 'nikola_md']
+
     def compile_html(self, source, dest):
         if markdown is None:
             raise Exception('To build this site, you need to install the "markdown" package.')
@@ -51,16 +53,7 @@ class CompileMarkdown(PageCompiler):
         with codecs.open(dest, "w+", "utf8") as out_file:
             with codecs.open(source, "r", "utf8") as in_file:
                 data = in_file.read()
-            output = markdown(data, ['fenced_code', 'codehilite'])
-            # h1 is reserved for the title so increment all header levels
-            for n in reversed(range(1,9)):
-                output = re.sub('<h%i>' % n, '<h%i>' % (n + 1), output)
-                output = re.sub('</h%i>' % n, '</h%i>' % (n + 1), output)
-            # python-markdown's highlighter uses the class 'codehilite' to wrap
-            # code, # instead of the standard 'code'. None of the standard
-            # pygments stylesheets use this class, so swap it to be 'code'
-            output = re.sub(r'(<div[^>]+class="[^"]*)codehilite([^>]+)',
-                            r'\1code\2', output)
+            output = markdown(data, self.extensions)
             out_file.write(output)
 
     def create_post(self, path, onefile=False, title="", slug="", date="", tags=""):
